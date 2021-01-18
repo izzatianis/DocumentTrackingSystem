a98e260e0d428f48e78897eb9983b866089a185f
izzatianis <shafiq_razman@yahoo.com>
2021-01-18 23:52:28 +0800

    second commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '635d0f0174283a3e0979cbcd770859e53324cac3',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '635d0f0174283a3e0979cbcd770859e53324cac3',
+    ),
+  ),
+);
